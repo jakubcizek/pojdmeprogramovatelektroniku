62c9c77a0351a5e22464ae0aa5540f4edd6b703c
Jakub Čížek <cizekuba@gmail.com>
2023-06-05 11:58:52 +0200

    Update ESP32_eink_GDEY073D46.ino

--- MeteoEinkDashboard/Arduino/ESP32_eink_GDEY073D46.ino
+++ MeteoEinkDashboard/Arduino/ESP32_eink_GDEY073D46.ino
@@ -1,4 +1,4 @@
-#include <WiFi.h> // PRipojeni k Wi-Fi
+#include <WiFi.h> // Pripojeni k Wi-Fi
 #include <HTTPClient.h> // HTTP klient; stazeni bitmapy
 #include "gdey073d46.h" // Hlavickovy soubor s funkcemi pro praci s e-inkem
 
@@ -10,36 +10,36 @@ const char* heslo = "WPA heslo k Wi-Fi";
 // Soubor vytvori generator v Pythonu, musite jej ale spoustet na vlastnim serveru
 String url = "http:// ???? /dashboard_rle.bin";
 
-RTC_DATA_ATTR uint32_t pocitadlo_probuzeni = 0; // Pocitadlo probuzeni (vypisuje se pro kontrolu do seriove lniky)
+RTC_DATA_ATTR uint32_t pocitadlo_probuzeni = 0; // Pocitadlo probuzeni (vypisuje se pro kontrolu do seriove linky)
 uint64_t prodleva_s = 3600; // Prodleva v sekundach mezi probuzenim hlavniho procesoru z hlubokeho spanku (1 hodina)
 
 // Funkce pro stazeni a dekomprimaci bitmapy do RAM
 uint32_t stahni_a_dekomprimuj() {
   uint32_t stazeno = 0;
   HTTPClient http;
-  http.setTimeout(10000); // Zvysimu timeout pro TCP spojeni na 10 sekund, pokud budeme bitmapu generovat pri HTTP pozadavku
+  http.setTimeout(10000); // Zvysimu timeout pro TCP spojeni na 10 sekund, pokud budeme bitmapu generovat pri HTTP pozadavku (muze byt velka prodleva, nez server zacne posilat data)
   http.begin(url); // Vytvorime HTTP pozadavek s nasi URL
   int kod = http.GET(); // Zaciname HTTP GET komunikaci
   Serial.printf(" (HTTP kod: %d) ", kod);
   if (kod == HTTP_CODE_OK) { // Pokud jsme navazali HTTP spojeni (odpoved HTTP OK)
     uint32_t velikost_soubor = http.getSize(); // Velikost dat ke stazeni
     Serial.printf(" (Ke stazeni: %d) ", velikost_soubor);
-    WiFiClient* stream = http.getStreamPtr(); // Otevreme strem na stahovana data
+    WiFiClient* stream = http.getStreamPtr(); // Otevreme stream na stahovana data
     uint32_t dekomprimovane_bajty = 0;
     while (http.connected() && (stazeno < velikost_soubor)) { // Pokud jsme spojeni se serverem a jeste jsme nestahli cely soubor
-      size_t k_dispozici = stream->available(); // Zjistime velikost aktualne stazenych dat v zasobniku
-      if (k_dispozici) { // Pokud tam nejake jsou
+      size_t k_dispozici = stream->available(); // Zjistime velikost aktualne stazenych dat ve streamovacim zasobniku
+      if (k_dispozici) { // Pokud tam neco je
         uint8_t buffer[128];
-        size_t precteno = stream->readBytes(buffer, 128); // Precteme 128 bajtu do naseho docasneho zasobniku
+        size_t precteno = stream->readBytes(buffer, 128); // Precteme 128 bajtu do naseho vlastniho maleho zasobniku (je to rychlejsi, nez cist bajt po bajtu)
         if (precteno) {
-          // RLE dekomprese davky v bufferu
-          // V nasem zasobniku musi byt sudy pocet bajtu, jinak mame problem!
+          // RLE dekomprese davky v zasobniku
+          // !!! V nasem zasobniku musi byt sudy pocet bajtu, jinak mame problem !!!
           for (size_t pozice = 0; pozice < precteno; pozice += 2) { // Precteme vzdy dva bajty z naseho zasobniku. Prvni bajt je pocet, druhy bajt hodnota
             uint8_t pocet = buffer[pozice];
             uint8_t hodnota = buffer[pozice + 1];
-            for (uint8_t pozice = 0; pozice < pocet; pozice++) bitmap_buffer[dekomprimovane_bajty++] = hodnota; // Vypnime bitmapu davkou
+            for (uint8_t pozice = 0; pozice < pocet; pozice++) bitmap_buffer[dekomprimovane_bajty++] = hodnota; // Vyplnime bitmapu davkou
           }
-          stazeno += precteno; // Navysime pocitaldo stzenych bajtu
+          stazeno += precteno; // Navysime pocitadlo stazenych bajtu
         }
       }
       delay(1); // Drobna cekaci prodleva pro zvyseni stability prenosu. Lze s tim experimentovat
@@ -52,7 +52,7 @@ uint32_t stahni_a_dekomprimuj() {
 
 // Hlavni funkce setup se spusti hned na zacatku
 void setup() {
-  Serial.begin(115200); // NAstartujeme seriovou linku
+  Serial.begin(115200); // Nastartujeme seriovou linku
   delay(1000); // Jen umela prodleva, abychom stacili otevrit seriovy terminal, pokud chceme
   // Konfigurace pomocnych signalu e-inku
   pinMode(BUSY_Pin, INPUT);
@@ -62,7 +62,10 @@ void setup() {
   // Nastartovani sbernice SPI
   SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE0));
   SPI.begin();
-  // Vytvoreni blok upameti pro bitmapu
+  // Vytvoreni bloku pameti pro bitmapu
+  // Pozor, blok pameti se alokuje v externi PSRAM
+  // Pokud nemate desku s modulem ESP32-WROVER, je treba kosmeticky upravit podobu funkce bitmap_psram_init (nahradit ps_malloc za standardni malloc),
+  // anebo vetsi merou prepsat kod tak, aby se nevytvarel lokalni frame buffer v RAM pro bitmapu
   Serial.printf("Alokuji %d bajtu v PSRAM pro 4bit bitmapu... ", BITMAP_SIZE);
   bitmap_psram_init();
   Serial.println("OK");
@@ -92,12 +95,14 @@ void setup() {
   // Prepneme e-ink do usporneho/vypnuteho rezimu 
   Serial.println("Vypinam e-ink...");
   eink_spanek();
+  
   // Vypneme Wi-Fi
   Serial.println("Odpojuji Wi-Fi...");
   WiFi.mode(WIFI_OFF);
+  
   // Prepneme se do rezimu hlubokeho spanku
   // Z nej se probudime za hodinu a cele kolecko se zopakuje
-  // Displej se tedy prekresluje jednou z ahodinu
+  // Displej se tedy prekresluje jednou za hodinu
   Serial.println("Prechazim do hlubokeho spanku...");
   Serial.flush();
   esp_sleep_enable_timer_wakeup(prodleva_s * 1000000ULL); // Registrace probuzeni timerem
@@ -105,7 +110,7 @@ void setup() {
 }
 
 
-// SMycka loop je prazdna, nikdy se totiz nespusti
+// Smycka loop je prazdna, nikdy se totiz nespusti
 // Cip ESP32 se probuzeni z hlubokeho spanku resetuje
 // a program se spusti od zacatku. Pocitadlo probuzeni prezije,
 // protoze jsme jej atributem RTC_DATA_ATTR ulozili do specialni a omezene RAM,
